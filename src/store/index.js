c2390c8ebc949144a6c47a8f476f1e42f580955b
David Pessanha <davidppessanha@gmail.com>
2021-09-28 11:04:14 -0300

    Updated vuex: created mutations and actions to change state of gifs arrayand show all gifs got from the api call

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,23 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import GifService from "@/services/GifService.js";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  state: {
+    gifs: [],
+  },
+  mutations: {
+    SET_GIFS(state, gifs) {
+      state.gifs = gifs;
+    },
+  },
+  actions: {
+    fetchGifs({ commit }, querySearch) {
+      return GifService.getGifs(querySearch).then((response) => {
+        commit("SET_GIFS", response.data.data);
+      });
+    },
+  },
 });
