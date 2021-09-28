bdddf1ca25076b17d63a2f77363f57c5350d8664
David Pessanha <davidppessanha@gmail.com>
2021-09-28 20:13:37 -0300

    Default gifs for empty query

--- src/services/GifService.js
+++ src/services/GifService.js
@@ -1,11 +1,19 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: `http://api.giphy.com/v1/gifs/search?api_key=${process.env.VUE_APP_ENV_VARIABLE}&q=`,
+  baseURL: `http://api.giphy.com/v1/gifs/`,
 });
+
+const getURL = (keyword, query, limit) => {
+  return `${keyword}?api_key=${process.env.VUE_APP_ENV_VARIABLE}&q=${query}&limit=${limit}`;
+};
 
 export default {
   getGifs(query, limit) {
-    return apiClient.get(query + "&limit=" + limit);
+    if (query) {
+      return apiClient.get(getURL("search", query, limit));
+    } else {
+      return apiClient.get(getURL("trending", query, limit));
+    }
   },
 };
