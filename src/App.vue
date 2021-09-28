499776feab1da2dde199ed4ea7a2e59c4a4f9584
David Pessanha <davidppessanha@gmail.com>
2021-09-28 10:20:16 -0300

    Created and used NavBar component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
-<template><div></div></template>
+<template>
+  <v-app>
+    <Navbar />
+  </v-app>
+</template>
 
 <script>
+import Navbar from "@/components/NavBar.vue";
+
 export default {
   name: "App",
-
+  components: {
+    Navbar,
+  },
   data: () => ({
     //
   }),
---
+++ src/components/NavBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app-bar app color="purple darken-4" dark>
+    <div class="d-flex align-center">
+      <h1>Giphy-like</h1>
+    </div>
+
+    <v-spacer></v-spacer>
+
+    <v-btn href="https://giphy.com/" target="_blank" text>
+      <span class="mr-2">Visit real giphy</span>
+      <v-icon>mdi-open-in-new</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
