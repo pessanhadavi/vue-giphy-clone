25712cd625fd3ee74fa820efa6cc3562106afb18
David Pessanha <davidppessanha@gmail.com>
2021-09-28 11:13:59 -0300

    Added input event on SearchInput component + dispatched fetchGifs action on store

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,7 +1,28 @@
 <template>
   <div>
     <v-form>
-      <v-text-field label="Search gif..." />
+      <v-text-field
+        label="Search gif..."
+        v-model="querySearch"
+        @input="search"
+      />
     </v-form>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      querySearch: "",
+    };
+  },
+  methods: {
+    search() {
+      this.$store.dispatch("fetchGifs", this.querySearch);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
