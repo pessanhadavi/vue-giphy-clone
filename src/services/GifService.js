47958285c7201678c4792acdbf721210e85866a0
David Pessanha <davidppessanha@gmail.com>
2021-09-28 10:56:37 -0300

    Created service file: GifService to make api calls related to giphy + created getGifs event to get 32 gifs by searching

---
+++ src/services/GifService.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL: `http://api.giphy.com/v1/gifs/search?api_key=${process.env.VUE_APP_ENV_VARIABLE}&q=`,
+});
+
+export default {
+  getGifs(query) {
+    return apiClient.get(query + "&limit=32");
+  },
+};
