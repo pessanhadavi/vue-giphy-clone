0fbf334bbe666690388e07bb23b80c91e13d4a78
David Pessanha <davidppessanha@gmail.com>
2021-09-28 19:03:19 -0300

    Created component for loader icon + animation

---
+++ src/components/Loader.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <v-icon class="load-icon" dark>mdi-timer-sand-empty</v-icon>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.load-icon {
+  -webkit-animation: spin 5s linear infinite;
+  animation: spin 5s linear infinite;
+}
+
+@-webkit-keyframes spin {
+  0% {
+    -webkit-transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+  }
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
