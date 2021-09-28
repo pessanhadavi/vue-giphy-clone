0a80d8a147bcec5ba711df0c71aebcfcee7bc028
David Pessanha <davidppessanha@gmail.com>
2021-09-28 11:11:36 -0300

    Accepted props coming to Gif Component

--- src/components/Gif.vue
+++ src/components/Gif.vue
@@ -1,9 +1,17 @@
 <template>
-  <div></div>
+  <div>
+    <a :href="gif.images.downsized.url">
+      <v-img :src="gif.images.downsized.url" height="100px" width="200px" />
+    </a>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    gif: Object,
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
