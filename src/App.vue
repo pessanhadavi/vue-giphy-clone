86cd6b0d40398000a077d3e6b55bf3332c32f6e7
David Pessanha <davidppessanha@gmail.com>
2021-09-28 10:26:46 -0300

    Created new view: GifList

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <v-app>
     <Navbar />
+    <v-main>
+      <GifList />
+    </v-main>
   </v-app>
 </template>
 
 <script>
 import Navbar from "@/components/NavBar.vue";
+import GifList from "@/views/GifList.vue";
 
 export default {
   name: "App",
   components: {
     Navbar,
+    GifList,
   },
   data: () => ({
     //
