4ad3c855a23c7d8458c4aba1fae31806739d9983
David Pessanha <davidppessanha@gmail.com>
2021-09-29 11:29:05 -0300

    Created basebutton component

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-btn
+    color="purple darken-4"
+    dark
+    x-large
+    width="300px"
+    v-on="$listeners"
+    v-bind="$attrs"
+  >
+    <slot />
+  </v-btn>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+};
+</script>
