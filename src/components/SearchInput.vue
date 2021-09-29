13927191c41ba359ffa749957220476b16ab2b44
David Pessanha <davidppessanha@gmail.com>
2021-09-29 11:29:46 -0300

    Inserted button component + changed event to on submit form

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="my-8">
-    <v-form>
-      <v-text-field
-        label="Search gif..."
-        color="purple darken-4"
-        v-model="querySearch"
-        @input="search"
-      />
+    <v-form @submit.prevent="search">
+      <v-row>
+        <v-col cols="12">
+          <v-text-field
+            label="Search gif..."
+            color="purple darken-4"
+            v-model="querySearch"
+          />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" class="d-flex justify-center">
+          <BaseButton type="submit">Search</BaseButton>
+        </v-col>
+      </v-row>
     </v-form>
   </div>
 </template>
 
 <script>
+import BaseButton from "@/components/BaseButton.vue";
+
 export default {
+  components: {
+    BaseButton,
+  },
   data() {
     return {
       querySearch: "",
