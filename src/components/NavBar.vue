4bcd5c65e48601c0a924b8cb41cdecb437447517
David Pessanha <davidppessanha@gmail.com>
2021-09-29 22:18:36 -0300

    minor adjustments

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,11 +1,9 @@
 <template>
   <v-app-bar id="nav-bar" app color="purple darken-4" dark>
     <div class="d-flex align-center">
-      <a href="/"> <h1>Giphy-like</h1></a>
+      <a href="/"><h1>Giphy-like</h1></a>
     </div>
-
     <v-spacer></v-spacer>
-
     <v-btn href="https://giphy.com/" target="_blank" text>
       <span class="mr-2">Visit real giphy</span>
       <v-icon>mdi-open-in-new</v-icon>
